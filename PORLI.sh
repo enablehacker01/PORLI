d2e328a0d1c538b52b7acd30636027a70327c8a2
QBZ <50372071+enablehacker01@users.noreply.github.com>
2019-09-29 02:06:40 +0700

    Add files via upload
    Script KIDDING

---
+++ PORLI.sh
@@ -0,0 +1,197 @@
+#! /bin/bash
+
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
+
+clear
+echo
+echo $red"          Tool akan di install dalam waktu 3 detik"
+echo $red"             KEPOLISIAN REPUBLIK INDONESIA :v("
+sleep 1
+echo '''\a
+\033[34;1m                              _
+\033[34;1m                             / |
+\033[34;1m                             | |
+\033[34;1m                             | |
+\033[34;1m                            _|_|_ '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"         Tool akan di install dalam waktu 3 detik"
+echo $red"             KEPOLISIAN REPUBLIK INDONESIA :v("
+sleep 1
+echo '''\a
+\033[32;1m                             ____
+\033[32;1m                            |___ \
+\033[32;1m                             __) |
+\033[32;1m                            / __/
+\033[32;1m                           |_____| '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"         Tool akan di install dalam waktu 3 detik"
+echo $red"             KEPOLISIAN REPUBLIK INDONESIA :v("
+sleep 1
+echo '''\a
+\033[35;1m                            _____
+\033[35;1m                           |___ /
+\033[35;1m                             |_ \
+\033[35;1m                            ___) |
+\033[35;1m                           |____/ '''
+sleep 3
+clear
+
+
+figlet HACKER | lolcat
+
+                    echo " Selamat Datang Di Server Indonesia "
+                    sleep 2
+					echo " Kamu Hanya Dapat Masuk 1 kali Dalam Setahun "
+					sleep 2
+					echo " Ketika Kamu Memaksa Untuk Masuk Kamu akan Mendapat Resiko Yang Besar "
+					sleep 2
+					echo " Ini Adalah Alat Untuk Mencari Informasi Seputar Identitas Seseorang "
+					sleep 2
+					echo " Jika Kamu di Di ketahui Kamu Akan DI Tangkap Oleh Kepolisian "
+					sleep 20
+					
+					echo " Selamat Datang  "
+					echo " Siapa nama Kamu ? "
+					read nama
+					
+					echo " SILAHKAN MASUK $nama "
+				    sleep 03
+                    clear
+                    
+                    echo " Apakah Anda Ingin mencari Identitas Seseorang ?[Y/N] "
+                    read Identitas
+                    
+                    echo " Silahkan Masukan Kata sandi Negara "
+                    read Kata
+ 
+clear
+echo
+echo $red"          Tool akan di install dalam waktu 3 detik"
+echo $red"            KEPOLISIAN REPUBLIK INDONESIA :v("
+sleep 1
+echo '''\a
+\033[34;1m                              _
+\033[34;1m                             / |
+\033[34;1m                             | |
+\033[34;1m                             | |
+\033[34;1m                            _|_|_ '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"         Tool akan di install dalam waktu 3 detik"
+echo $red"             KEPOLISIAN REPUBLIK INDONESIA :v("
+sleep 1
+echo '''\a
+\033[32;1m                             ____
+\033[32;1m                            |___ \
+\033[32;1m                             __) |
+\033[32;1m                            / __/
+\033[32;1m                           |_____| '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"         Tool akan di install dalam waktu 3 detik"
+echo $red"             KEPOLISIAN REPUBLIK INDONESIA :v"
+sleep 1
+echo '''\a
+\033[35;1m                            _____
+\033[35;1m                           |___ /
+\033[35;1m                             |_ \
+\033[35;1m                            ___) |
+\033[35;1m                           |____/ '''
+sleep 3
+clear              
+				
+					echo " Sukses "
+					echo " silahkan Masukan no NIK KTP/PASSPORT/NIK(Kartu Keluarga)"
+					read Masukan
+					sleep 3
+					
+echo
+echo $red"          Tool akan di install dalam waktu 3 detik"
+echo $red"             KEPOLISIAN REPUBLIK INDONESIA :v("
+sleep 1
+echo '''\a
+\033[34;1m                              _
+\033[34;1m                             / |
+\033[34;1m                             | |
+\033[34;1m                             | |
+\033[34;1m                            _|_|_ '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"         Tool akan di install dalam waktu 3 detik"
+echo $red"            KEPOLISIAN REPUBLIK INDONESIA :v"
+sleep 1
+echo '''\a
+\033[32;1m                             ____
+\033[32;1m                            |___ \
+\033[32;1m                             __) |
+\033[32;1m                            / __/
+\033[32;1m                           |_____| '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"         Tool akan di install dalam waktu 3 detik"
+echo $red"             KEPOLISIAN REPUBLIK INDONESIA :v("
+sleep 1
+echo '''\a
+\033[35;1m                            _____
+\033[35;1m                           |___ /
+\033[35;1m                             |_ \
+\033[35;1m                            ___) |
+\033[35;1m                           |____/ '''
+sleep 3              					
+					
+				
+	clear				
+				
+echo "[+]─────────────────────────────────────────────────────[+]" | lolcat
+echo " |         ••••••••••|KARTU TANDA PENDUDUK | ••••••••••        |" | lolcat
+echo " | ─────────────────────────────────────────────────────       |" | lolcat
+echo " |                    PROVINSI DKI JAKARTA                     |" | lolcat
+echo " |                      JAKARTA SELATAN\                       |" | lolcat
+echo " | NIK              : 3216061106980033                         |" | lolcat
+echo " | Nama             : ADE CALVIN ARDIANSYAH                    |" | lolcat
+echo " | Tempat/tgl.Lahir : BEKASI,11-06-2=1998                      |" | lolcat
+echo " | Alamat           : KP KEDUNG GEDE NO 92                     |" | lolcat
+echo " | RT/RW            : 002/015                                  |" | lolcat
+echo " | Kel/desa         : SETIA MEKAR                              |" | lolcat
+echo " | Kecamatan        : TAMBUN SELATAN                           |" | lolcat
+echo " | Agama            : ISLAM                                    |" | lolcat
+echo " | Status           : Belum Kawin                              |" | lolcat
+echo " | Pekerjaan        : Pelajar/Mahasiswa                        |" | lolcat
+echo " | Kewarganegaran   : WNI                                      |" | lolcat
+echo " | Berlaku Hingga   : SEUMUR HIDUP                             |" | lolcat
+echo "[+]─────────────────────────────────────────────────────[+]" | lolcat
+
+echo " TERIMA KASIH "
+echo "Silahkan Keluar"
